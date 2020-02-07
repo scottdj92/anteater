ca5b1daf0d997396a2b1ee68a0c2c600f7715a75
Scott Jones <scottdj92@gmail.com>
2020-02-06 22:07:35 -0500

    refactor to use URL()

--- eat.js
+++ eat.js
@@ -9,6 +9,8 @@ const blacklist = [
     "mkwid",
     // facebook
     "ad_id",
+    "loc",
+    "ls_ref",
     "adset_id",
     "campaign_id",
     "ad_name",
@@ -20,18 +22,14 @@ const blacklist = [
 ];
 
 document.addEventListener("copy", (doc) => {
-    const url = decodeURIComponent(doc.target.value);
-    if (url !== "undefined") {
+    try {
+        const url = new URL(doc.target.value);
         doc.preventDefault();
-        const params = url.split("?")[1];
-        const keys = params.split("&");
-        // we have all of the params split by key=val
-        // check to see if any one of them matches the blacklist. if it does, remove it.
+        const keys = url.search.split("?")[1].split("&");
         const saved = keys.filter((key) => !blacklist.includes(key.split("=")[0]));
-        // re-join all values
-        const joined = saved.join("&");
-        const full = url.split("?")[0] + joined;
-        // now save to latest clipboard entry
+        const full = `${url.origin}${url.pathname}${saved.join("&")}`;
         navigator.clipboard.writeText(full);
+    } catch (err) {
+        void(0);
     }
 });
